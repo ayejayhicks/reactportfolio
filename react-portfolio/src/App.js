6fa2dce8ee3c65b52393fb3a28db9741a8037ff3
Alaina Hicks <ajhicks0816@gmail.com>
2021-05-12 16:07:05 -0500

    create pages

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -6,6 +6,9 @@ import Nav from "react-bootstrap/Nav";
 import './App.css';
 
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
 class App extends React.Component {
 
@@ -52,10 +55,15 @@ class App extends React.Component {
                 <Link className="nav-link" to="/">About</Link>
                 <Link className="nav-link" to="/">Resume</Link>
                 <Link className="nav-link" to="/">Contact</Link>
+
               </Nav>
             </Navbar.Collapse>
           </Navbar>
 
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/" exact render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/" exact render={() => <ContactPage title={this.state.contact.title} />} />
+ 
           <Footer />
 
             </Container>
