66e50a626171c7736f9061c88240199fc537f7b2
Alaina Hicks <ajhicks0816@gmail.com>
2021-05-12 13:11:06 -0500

    extra README deletion, components folder added, footer developed

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-
 import './App.css';
+
+import Footer from "./components/Footer";
 
 class App extends React.Component {
 
@@ -34,30 +35,33 @@ class App extends React.Component {
     };
   }
 
-      render() {
-        return (
-          <Router>
-            <Container className="p-0" fluid={true}>
-              
-              <Navbar className="border-bottom" bg="transparent" expand="lg">
-                <Navbar.Brand>Alaina J. Hicks</Navbar.Brand>
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
 
-                <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-                <Navbar.Collapse id="navbar-toggle">
-                  <Nav className="ml-auto">
-                    <Link className="nav-link" to="/">Home</Link>
-                    <Link className="nav-link" to="/">About</Link>
-                    <Link className="nav-link" to="/">Resume</Link>
-                    <Link className="nav-link" to="/">Contact</Link>
-                  </Nav>
-                  </Navbar.Collapse> 
-              </Navbar>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Alaina J. Hicks</Navbar.Brand>
 
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+
+              {/* //Navigation bar */}
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/">About</Link>
+                <Link className="nav-link" to="/">Resume</Link>
+                <Link className="nav-link" to="/">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Footer />
 
             </Container>
-          </Router>
+          </Router >
         );
-      }
-    }
+  }
+}
 
-    export default App;
+export default App;
