9528bd239ca86cb7731200a1f6a1d5e03109c02e
Alaina Hicks <ajhicks0816@gmail.com>
2021-05-26 23:38:37 -0500

    removed resume like, portfolio-api

---
+++ Portfolio-API/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('bodyParser');
+const cors = require('cors');
+
+const sendGrid = require('@sendGrid/mail');
+
+
+const app = express();
--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ class App extends React.Component {
       headerLinks: [
         { title: "Home", path: '/' },
         { title: "About", path: '/about' },
-        { title: "Resume", path: '/resume' },
         { title: "Contact", path: '/contact' },
       ],
       home: {
